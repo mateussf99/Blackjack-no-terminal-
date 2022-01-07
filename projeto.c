ed844de30a1646ee2b53416e7ec4798c303e5ec1
mateussf99 <mateus.ms65@gmail.com>
2022-01-07 17:36:46 -0300

    atualizacao_final

--- projeto.c
+++ projeto.c
@@ -36,9 +36,258 @@ Lista* criar_lista2() // lista para guarda as cartas puxadas da banca
 
     return banca;
 }
-
-void rodada_inicial(Lista* jogador, Lista *banca, int *soma1, int *soma2)
+void resultado(Lista* jogador, Lista *banca, int *soma1, int *soma2)
 {
+    limpar();
+    No* atual;
+    if (*soma2 == *soma1)
+    {
+        printf("A Banca puxou algumas cartas\n");
+        printf("jogador: ");
+        for(atual = jogador->inicio; atual != NULL; atual = atual->next)
+        {
+            printf("%d, ", atual->valor);
+        }
+        printf("\n");
+        printf("%d\n", *soma1);
+        printf("Banca: ");
+        for(atual = banca->inicio; atual != NULL; atual = atual->next)
+        {
+            printf("%d, ", atual->valor);
+        }
+        printf("\n");
+        printf("%d\n", *soma2);
+        printf("DEU EMPATE!!!!!\n");
+    }
+    else if (*soma2 < *soma1 && *soma1 <= 21)
+    {
+        printf("A Banca puxou algumas cartas\n");
+        printf("jogador: ");
+        for(atual = jogador->inicio; atual != NULL; atual = atual->next)
+        {
+            printf("%d, ", atual->valor);
+        }
+        printf("\n");
+        printf("%d\n", *soma1);
+        printf("Banca: ");
+        for(atual = banca->inicio; atual != NULL; atual = atual->next)
+        {
+            printf("%d, ", atual->valor);
+        }
+        printf("\n");
+        printf("%d\n", *soma2);
+        printf("O JOGADOR GANHOU!!!!\n");
+    }
+    else if (*soma2 > *soma1 && *soma1 <= 21 && *soma2 > 21)
+    {
+        printf("A Banca puxou algumas cartas\n");
+        printf("jogador: ");
+        for(atual = jogador->inicio; atual != NULL; atual = atual->next)
+        {
+            printf("%d, ", atual->valor);
+        }
+        printf("\n");
+        printf("%d\n", *soma1);
+        printf("Banca: ");
+        for(atual = banca->inicio; atual != NULL; atual = atual->next)
+        {
+            printf("%d, ", atual->valor);
+        }
+        printf("\n");
+        printf("%d\n", *soma2);
+        printf("O JOGADOR GANHOU!!!!\n");
+    }
+    else if (*soma2 > *soma1 && *soma2 < 21)
+    {
+        printf("A Banca puxou algumas cartas\n");
+        printf("jogador: ");
+        for(atual = jogador->inicio; atual != NULL; atual = atual->next)
+        {
+            printf("%d, ", atual->valor);
+        }
+        printf("\n");
+        printf("%d\n", *soma1);
+        printf("Banca: ");
+        for(atual = banca->inicio; atual != NULL; atual = atual->next)
+        {
+            printf("%d, ", atual->valor);
+        }
+        printf("\n");
+        printf("%d\n", *soma2);
+        printf("A BANCA GANHOU!!!!\n");
+    }
+    else if (*soma2 > *soma1 && *soma2 <= 21)
+    {
+        printf("A Banca puxou algumas cartas\n");
+        printf("jogador: ");
+        for(atual = jogador->inicio; atual != NULL; atual = atual->next)
+        {
+            printf("%d, ", atual->valor);
+        }
+        printf("\n");
+        printf("%d\n", *soma1);
+        printf("Banca: ");
+        for(atual = banca->inicio; atual != NULL; atual = atual->next)
+        {
+            printf("%d, ", atual->valor);
+        }
+        printf("\n");
+        printf("%d\n", *soma2);
+        printf("A BANCA GANHOU!!!!\n");
+    }
+    else if (*soma2 < *soma1 && *soma1 > 21)
+    {
+        printf("jogador: ");
+        for(atual = jogador->inicio; atual != NULL; atual = atual->next)
+        {
+            printf("%d, ", atual->valor);
+        }
+        printf("\n");
+        printf("%d\n", *soma1);
+        printf("Banca: ");
+        for(atual = banca->inicio; atual != NULL; atual = atual->next)
+        {
+            printf("%d, ", atual->valor);
+        }
+        printf("\n");
+        printf("%d\n", *soma2);
+        printf("A BANCA GANHOU!!!!\n");
+    }
+    
+}
+void ad_jogador(Lista* jogador,  int *soma1)
+{
+    srand(time(NULL));
+    No *novo = malloc(sizeof(No));
+    novo->valor = rand() % 11;
+    if (jogador->inicio == NULL)
+    {
+        jogador->inicio = novo; 
+        novo->next = NULL;
+        *soma1 += novo->valor;
+    }
+    else
+    {
+        novo->next = jogador->inicio;
+        jogador->inicio=novo;
+        *soma1 += novo->valor;
+    }
+}
+void ad_banca(Lista* jogador, Lista *banca, int *soma1, int *soma2)
+{
+    int cont=2;
+    srand(time(NULL));
+    while(cont != 5)
+    {
+        limpar();
+        if(*soma2 < *soma1 && *soma1 <= 21)
+        {
+            No *novo = malloc(sizeof(No));
+            novo->valor = rand() % 11;
+            if (banca->inicio == NULL)
+            {
+                banca->inicio = novo; 
+                novo->next = NULL;
+                *soma2 += novo->valor;
+            }
+            else
+            {
+                novo->next = banca->inicio;
+                banca->inicio=novo;
+                *soma2 += novo->valor;
+            }
+            No* atual;
+            printf("jogador: ");
+            for(atual = jogador->inicio; atual != NULL; atual = atual->next)
+            {
+                printf("%d, ", atual->valor);
+            }
+            printf("\n");
+            printf("%d\n", *soma1);
+            printf("Banca: ");
+            for(atual = banca->inicio; atual != NULL; atual = atual->next)
+            {
+                printf("%d, ", atual->valor);
+            }
+            printf("\n");
+            printf("%d\n", *soma2);
+            cont++;
+        }
+        else
+        {
+            break;
+        }
+        
+    }
+    resultado(jogador, banca, soma1, soma2);
+    
+}
+
+void carta(Lista* jogador, Lista *banca, int *soma1, int *soma2)
+{
+    int escolha, cont=2;
+    while(escolha != 2)
+    {
+        printf("------------------------------\n");
+        printf("|1| Pedir carta\n");
+        printf("|2| Passar a vez para a Banca\n");
+        printf("------------------------------\n\n");
+
+        printf("Escolha: ");
+        scanf("%d", &escolha);
+        getchar();
+        limpar();
+        switch(escolha) 
+        {
+            case 1:
+                if(cont<5)
+                {
+                    cont++;
+                    ad_jogador(jogador,  soma1);
+                    No* atual;
+                    printf("jogador: ");
+                    for(atual = jogador->inicio; atual != NULL; atual = atual->next)
+                    {
+                        printf("%d, ", atual->valor);
+                    }
+                    printf("\n");
+                    printf("%d\n", *soma1);
+                    printf("Banca: ");
+                    for(atual = banca->inicio; atual != NULL; atual = atual->next)
+                    {
+                        printf("%d, ", atual->valor);
+                    }
+                    printf("\n");
+                    printf("%d\n", *soma2);
+                }
+                else
+                {
+                    printf("Voçê não pode pedir mais cartas\n");
+                }
+                break;
+            case 2:
+                ad_banca(jogador, banca, soma1, soma2);
+                return;
+
+        default:
+            printf("ERRO: OPÇÃO INVÁLIDA\n");
+            break;
+        }
+        printf("\nPressione Enter para continuar...");
+        getchar();
+    }
+    
+
+}
+
+void rodada_inicial()
+{
+    int a = 0, b = 0;
+    int *soma1, *soma2;
+    soma1 = &a;
+    soma2 = &b;
+    Lista *jogador = criar_lista();
+    Lista *banca = criar_lista2();
     srand(time(NULL));
     for(int i=0; i<2; i++ ) //gera os numeros aleatorios para nossa rodada
     {
@@ -74,18 +323,7 @@ void rodada_inicial(Lista* jogador, Lista *banca, int *soma1, int *soma2)
             *soma2 += novo->valor;
         }
     }
-    
-    
-}
-
-void imprimir(Lista* jogador, Lista *banca, int *soma1, int *soma2)// criei so para ver se tava salvando certo
-{
     No* atual;
-    if(jogador->inicio == NULL)
-    {
-        printf("A lista está vazia.");
-        return;
-    }
     printf("jogador: ");
     for(atual = jogador->inicio; atual != NULL; atual = atual->next)
     {
@@ -100,16 +338,12 @@ void imprimir(Lista* jogador, Lista *banca, int *soma1, int *soma2)// criei so p
     }
     printf("\n");
     printf("%d\n", *soma2);
+    carta( jogador, banca, soma1, soma2);
 }
 
 void menu()
 {
-    int escolha, a = 0, b = 0;
-    int *soma1, *soma2;
-    soma1 = &a;
-    soma2 = &b;
-    Lista *jogador = criar_lista();
-    Lista *banca = criar_lista2();
+    int escolha;
 
     do
     {
@@ -119,8 +353,7 @@ void menu()
         printf("############ MENU ############\n");
         printf("------------------------------\n");
         printf("|1| Começar a jogar\n");
-        printf("|2| imprimir\n");
-        printf("|3| Encerrar\n");
+        printf("|2| Encerrar\n");
         printf("------------------------------\n\n");
 
         printf("Escolha: ");
@@ -130,12 +363,9 @@ void menu()
         switch(escolha) 
         {
             case 1:
-                rodada_inicial(jogador, banca, soma1, soma2);
+                rodada_inicial();
                 break;
             case 2:
-                imprimir(jogador, banca, soma1, soma2);
-                break;
-            case 3:
             return;
 
         default:
@@ -145,7 +375,7 @@ void menu()
         printf("\nPressione Enter para continuar...");
         getchar(); 
     }
-    while(escolha != 3);
+    while(escolha != 2);
 
 }
 
